ba79b9de89313f368e165b41f6a3b253c937e2ac
Bishu Kumar <75970614+bishu52490@users.noreply.github.com>
2022-01-28 11:32:03 +0530

    1st

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ export default function App() {
   return (
     <div className="App">
       <Header />
+      <Home />
       <Routes>
         <Route path="/" component={<Home />} />
       </Routes>
